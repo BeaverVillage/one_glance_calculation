ea49b5268a21a4c2e194ce649208e94fb894cfed
BeaverVillage <harrykjw398@gmail.com>
2026-06-23 22:24:02 +0900

    ㅂㄱ

--- assets/js/parking-fee-check-init.js
+++ assets/js/parking-fee-check-init.js
@@ -1,5 +1,7 @@
+import { initToolDrawer } from './tool-drawer.js?v=20260623-tool-drawer-all-calculators';
 import { initParkingBudgetMap } from './parking-budget-map.js?v=20260622-parking-nonads-perf-safe';
 
 document.addEventListener('DOMContentLoaded', () => {
+  initToolDrawer();
   initParkingBudgetMap();
 });
